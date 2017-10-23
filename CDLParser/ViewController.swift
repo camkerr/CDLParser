f74dc22ca370fdf079e83e073baf8fd2478bb818
Cameron Kerr <cameronkerr@Camerons-MacBook-Pro.local>
2017-10-23 14:18:08 -0400

    Initial Commit

---
+++ CDLParser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CDLParser
+//
+//  Created by Cameron Kerr on 2017-10-23.
+//  Copyright © 2017 Cameron Kerr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
